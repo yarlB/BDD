985c2daa0fa87b70638363426f15a4fa49115db6
yarl <yarl-baudig@mailoo.org>
2014-02-06 10:14:27 +0100

    .

--- Projet.sql
+++ Projet.sql
@@ -1,4 +1,4 @@
-DROP TABLE Femme CASCADE CONSTRAINT
+DROP TABLE Femme CASCADE CONSTRAINT;
 CREATE TABLE Femme (
        id_femme varchar2(20),
        nom varchar2(20),
@@ -10,7 +10,7 @@ CREATE TABLE Femme (
        PRIMARY KEY (id_femme)
 );
 
-DROP TABLE Adresse CASCADE CONSTRAINT
+DROP TABLE Adresse CASCADE CONSTRAINT;
 CREATE TABLE Adresse (
        id_adresse varchar2(20),
        adresse varchar2(20),
@@ -23,7 +23,7 @@ CREATE TABLE Adresse (
        PRIMARY KEY (id_adresse) 
 );
 
-DROP TABLE Demande_Hebergement CASCADE CONSTRAINT
+DROP TABLE Demande_Hebergement CASCADE CONSTRAINT;
 CREATE TABLE Demande_Hebergement (
        id_hebergement varchar2(20),
        id_femme varchar2(20),
@@ -33,92 +33,98 @@ CREATE TABLE Demande_Hebergement (
        besoin_logement_adapte varchar2(20),
        date_reponse_positive date,
        PRIMARY KEY (id_femme, id_adresse, id_hebergement),
-       FOREIGN KEY id_femme REFERENCES Femme(id_femme),
-       FOREIGN KEY id_adresse REFERENCES Adresse(id_adresse)
+       FOREIGN KEY (id_femme) REFERENCES Femme(id_femme),
+       FOREIGN KEY (id_adresse) REFERENCES Adresse(id_adresse)
 ); 
 
-DROP TABLE Habite CASCADE CONSTRAINT
+DROP TABLE Habite CASCADE CONSTRAINT;
 CREATE TABLE Habite (
        id_femme varchar2(20),
        id_adresse varchar2(20),
        date_emmenagement date,
        date_demenagement date,
        PRIMARY KEY (id_femme, id_adresse, date_emmenagement),
-       FOREIGN KEY id_femme REFERENCES Femme(id_femme),
-       FOREIGN KEY id_adresse REFERENCES Adresse(id_adresse)
+       FOREIGN KEY (id_femme) REFERENCES Femme(id_femme),
+       FOREIGN KEY (id_adresse) REFERENCES Adresse(id_adresse)
 );
 
-DROP TABLE Enfant CASCADE CONSTRAINT
+DROP TABLE Enfant CASCADE CONSTRAINT;
 CREATE TABLE Enfant (
        id_enfant varchar2(20),
        id_femme varchar2(20),
        nom varchar2(20),
        prenom varchar2(20),
-       sexe boolean, /* VRAI F; FAUX M */
+       sexe char(1), /* F/M */
        nationalite varchar2(20),
-       scolarise boolean,
+       scolarise char(1), /* O/N */
        date_naissance date,
-       a_charge boolean,
-       habite_avec_maman boolean
+       a_charge char(1), /* O/N */
+       habite_avec_maman char(1), /* O/N */
        PRIMARY KEY (id_enfant),
-       FOREIGN KEY id_femme REFERENCES Femme(id_femme)
+       FOREIGN KEY (id_femme) REFERENCES Femme(id_femme)
 );
 
-DROP TABLE Contact CASCADE CONSTRAINT
+DROP TABLE Contact CASCADE CONSTRAINT;
 CREATE TABLE Contact (
        id_femme varchar2(20),
        id_contact varchar2(20),
-       commentaires text,
+       commentaires varchar2(1000),
        description varchar2(20),
        date_c date,
        PRIMARY KEY (id_femme, id_contact),
-       FOREIGN KEY id femme REFERENCES Femme(id_femme)
+       FOREIGN KEY (id_femme) REFERENCES Femme(id_femme)
 ); 
 
-DROP TABLE Orientation CASCADE CONSTRAINT
+DROP TABLE Orientation CASCADE CONSTRAINT;
 CREATE TABLE Orientation (
        id_femme varchar2(20),
        description_o varchar2(20),
        PRIMARY KEY (id_femme, description_o),
-       FOREIGN KEY id_femme REFERENCES Femme(id_femme)
+       FOREIGN KEY (id_femme) REFERENCES Femme(id_femme)
 );
 
-DROP TABLE Situation_Professionnelle CASCADE CONSTRAINT
+DROP TABLE Situation_Professionnelle CASCADE CONSTRAINT;
 CREATE TABLE Situation_Professionnelle (
        id_femme varchar2(20),
        date_sp date,
-       description varchar2(20)
-       PRIMARY KEY (id_femme, date_sp),
-       FOREIGN KEY id_femme REFERENCES Femme(id_femme)
+       description varchar2(20),
+       PRIMARY KEY (id_femme,date_sp),
+       FOREIGN KEY (id_femme) REFERENCES Femme(id_femme)
 );
 
-DROP TABLE Ressources CASCADE CONSTRAINT
+
+
+DROP TABLE Ressources CASCADE CONSTRAINT;
 CREATE TABLE Ressources (
        id_femme varchar2(20),
        date_ress date,
        description_r varchar2(20),
        PRIMARY KEY (id_femme, date_ress, description_R),
-       FOREIGN KEY id_femme REFERENCES Femme(id_femme) 
+       FOREIGN KEY (id_femme) REFERENCES Femme(id_femme) 
 ); 
 
-DROP TABLE Situation_Maritale CASCADE CONSTRAINT
+
+DROP TABLE Situation_Maritale CASCADE CONSTRAINT;
 CREATE TABLE  Situation_Maritale (
        id_femme varchar2(20),
        date_sm date,
        description_sp varchar2(20),
        PRIMARY KEY (id_femme, date_sm),
-       FOREIGN KEY id_femme REFERENCES Femme(id_femme)
+       FOREIGN KEY (id_femme) REFERENCES Femme(id_femme)
 );
 
-DROP TABLE Telephone_Portable CASCADE CONSTRAINT
+
+
+DROP TABLE Telephone_Portable CASCADE CONSTRAINT;
 CREATE TABLE Telephone_Portable (
        id_femme varchar2(20),
        numero varchar2(10),
        date_tp date,
        PRIMARY KEY (id_femme,numero),
-       FOREIGN KEY id_femme REFERENCES Femme(id_femme)
-);     
+       FOREIGN KEY (id_femme) REFERENCES Femme(id_femme)
+);
 
+select table_name from user_tables;
 
 /*prout*/
 /*de mammouth*/
